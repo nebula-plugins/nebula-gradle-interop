4bb771043893803dfddfd3aca5c22e6510c7de46
Danny Thomas <dannyt@netflix.com>
2017-03-28 17:55:17 -0700

    Add VersionWithSelector that satisfies the Version and Comparator<Version> interfaces to hide parsing

---
+++ src/main/kotlin/com/netflix/nebula/interop/versions.kt
@@ -0,0 +1,24 @@
+package com.netflix.nebula.interop
+
+import org.gradle.api.internal.artifacts.ivyservice.ivyresolve.strategy.*
+
+private val VERSIONED_COMPARATOR = DefaultVersionComparator()
+private val VERSION_COMPARATOR = VERSIONED_COMPARATOR.asVersionComparator()
+private val VERSION_SCHEME = DefaultVersionSelectorScheme(VERSIONED_COMPARATOR)
+private val VERSION_PARSER = VersionParser()
+
+data class VersionWithSelector(val stringVersion: String, val version: Version = VERSION_PARSER.transform(stringVersion)) : Comparable<Version>, Version by version {
+    private val selector: VersionSelector by lazy {
+        VERSION_SCHEME.parseSelector(stringVersion)!!
+    }
+
+    override fun toString(): String =
+            stringVersion
+
+    fun asSelector(): VersionSelector = selector
+
+    fun asString(): String = stringVersion
+
+    override fun compareTo(other: Version): Int =
+            VERSION_COMPARATOR.compare(this, other)
+}
