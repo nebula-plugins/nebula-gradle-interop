79dc1715df87a65fe582d1389bb3db5c49cb9499
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2019-09-09 09:44:31 -0700

    Remove usage of deprecated DefaultVersionSelectorScheme constructor. Closes #5

--- src/main/kotlin/com/netflix/nebula/interop/versions.kt
+++ src/main/kotlin/com/netflix/nebula/interop/versions.kt
@@ -4,8 +4,8 @@ import org.gradle.api.internal.artifacts.ivyservice.ivyresolve.strategy.*
 
 private val VERSIONED_COMPARATOR = DefaultVersionComparator()
 private val VERSION_COMPARATOR = VERSIONED_COMPARATOR.asVersionComparator()
-private val VERSION_SCHEME = DefaultVersionSelectorScheme(VERSIONED_COMPARATOR)
 private val VERSION_PARSER = VersionParser()
+private val VERSION_SCHEME = DefaultVersionSelectorScheme(VERSIONED_COMPARATOR, VERSION_PARSER)
 
 data class VersionWithSelector(val stringVersion: String, val version: Version = VERSION_PARSER.transform(stringVersion)) : Comparable<Version>, Version by version {
     private val selector: VersionSelector by lazy {
