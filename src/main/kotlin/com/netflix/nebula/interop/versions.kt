474dd3ced1ac1c7ab09ba47a6fdb8ac47a36e291
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2019-09-10 17:56:39 -0700

    go back to default VERSION_SCHEME

--- src/main/kotlin/com/netflix/nebula/interop/versions.kt
+++ src/main/kotlin/com/netflix/nebula/interop/versions.kt
@@ -7,11 +7,11 @@ private val VERSIONED_COMPARATOR = DefaultVersionComparator()
 private val VERSION_COMPARATOR = VERSIONED_COMPARATOR.asVersionComparator()
 private val VERSION_PARSER = VersionParser()
 private val VERSION_SELECTOR_SCHEME_CLASS = DefaultVersionSelectorScheme::class.java
+private val VERSION_SCHEME = if(GradleVersion.current().versionGreaterThan("5.0")) VERSION_SELECTOR_SCHEME_CLASS.getConstructor(VersionComparator::class.java).newInstance(VERSIONED_COMPARATOR) else VERSION_SELECTOR_SCHEME_CLASS.getConstructor(VersionComparator::class.java, VersionParser::class.java).newInstance(VERSIONED_COMPARATOR, VERSION_PARSER)
 
 data class VersionWithSelector(val stringVersion: String, val version: Version = VERSION_PARSER.transform(stringVersion)) : Comparable<Version>, Version by version {
     private val selector: VersionSelector by lazy {
-        val versionScheme = if(GradleVersion.current().versionGreaterThan("5.0")) VERSION_SELECTOR_SCHEME_CLASS.getConstructor(VersionComparator::class.java).newInstance(VERSIONED_COMPARATOR) else VERSION_SELECTOR_SCHEME_CLASS.getConstructor(VersionComparator::class.java, VersionParser::class.java).newInstance(VERSIONED_COMPARATOR, VERSION_PARSER)
-        versionScheme.parseSelector(stringVersion)!!
+        VERSION_SCHEME.parseSelector(stringVersion)!!
     }
 
     override fun toString(): String =
