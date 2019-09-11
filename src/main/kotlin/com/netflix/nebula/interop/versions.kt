9444098f277e65d2df48874d80802383221ede19
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2019-09-11 09:45:06 -0700

    fix GradleVersion comparison in VersionWithSelector for DefaultVersionSelectorScheme

--- src/main/kotlin/com/netflix/nebula/interop/versions.kt
+++ src/main/kotlin/com/netflix/nebula/interop/versions.kt
@@ -7,7 +7,10 @@ private val VERSIONED_COMPARATOR = DefaultVersionComparator()
 private val VERSION_COMPARATOR = VERSIONED_COMPARATOR.asVersionComparator()
 private val VERSION_PARSER = VersionParser()
 private val VERSION_SELECTOR_SCHEME_CLASS = DefaultVersionSelectorScheme::class.java
-private val VERSION_SCHEME = if(GradleVersion.current().versionGreaterThan("5.0")) VERSION_SELECTOR_SCHEME_CLASS.getConstructor(VersionComparator::class.java).newInstance(VERSIONED_COMPARATOR) else VERSION_SELECTOR_SCHEME_CLASS.getConstructor(VersionComparator::class.java, VersionParser::class.java).newInstance(VERSIONED_COMPARATOR, VERSION_PARSER)
+private val VERSION_SCHEME = if(GradleVersion.current().versionGreaterThan("5.0"))
+        VERSION_SELECTOR_SCHEME_CLASS.getConstructor(VersionComparator::class.java, VersionParser::class.java).newInstance(VERSIONED_COMPARATOR, VERSION_PARSER)
+    else
+        VERSION_SELECTOR_SCHEME_CLASS.getConstructor(VersionComparator::class.java).newInstance(VERSIONED_COMPARATOR)
 
 data class VersionWithSelector(val stringVersion: String, val version: Version = VERSION_PARSER.transform(stringVersion)) : Comparable<Version>, Version by version {
     private val selector: VersionSelector by lazy {
