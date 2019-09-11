cafb82dd7c6032ebac3f3dc075aa2696889a5195
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2019-09-10 17:39:45 -0700

    support for old versions of gradle using reflection

--- src/main/kotlin/com/netflix/nebula/interop/versions.kt
+++ src/main/kotlin/com/netflix/nebula/interop/versions.kt
@@ -1,15 +1,17 @@
 package com.netflix.nebula.interop
 
 import org.gradle.api.internal.artifacts.ivyservice.ivyresolve.strategy.*
+import org.gradle.api.invocation.Gradle
 
 private val VERSIONED_COMPARATOR = DefaultVersionComparator()
 private val VERSION_COMPARATOR = VERSIONED_COMPARATOR.asVersionComparator()
 private val VERSION_PARSER = VersionParser()
-private val VERSION_SCHEME = DefaultVersionSelectorScheme(VERSIONED_COMPARATOR)
+private val VERSION_SELECTOR_SCHEME_CLASS = DefaultVersionSelectorScheme::class.java
 
-data class VersionWithSelector(val stringVersion: String, val version: Version = VERSION_PARSER.transform(stringVersion)) : Comparable<Version>, Version by version {
+data class VersionWithSelector(val stringVersion: String, val version: Version = VERSION_PARSER.transform(stringVersion), val gradle: Gradle? = null) : Comparable<Version>, Version by version {
     private val selector: VersionSelector by lazy {
-        VERSION_SCHEME.parseSelector(stringVersion)!!
+        val versionScheme = if(gradle == null || gradle.versionLessThan("5.0")) VERSION_SELECTOR_SCHEME_CLASS.getConstructor(VersionComparator::class.java).newInstance(VERSIONED_COMPARATOR) else VERSION_SELECTOR_SCHEME_CLASS.getConstructor(VersionComparator::class.java, VersionParser::class.java).newInstance(VERSIONED_COMPARATOR, VERSION_PARSER)
+        versionScheme.parseSelector(stringVersion)!!
     }
 
     override fun toString(): String =
---
+++ src/test/kotlin/com/netflix/nebula/interop/VersionsTest.kt
@@ -0,0 +1,43 @@
+package com.netflix.nebula.interop
+
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.whenever
+import org.gradle.api.internal.artifacts.ivyservice.ivyresolve.strategy.VersionParser
+import org.gradle.api.invocation.Gradle
+import org.junit.Before
+import org.junit.Test
+
+class VersionsTest {
+    lateinit var gradle: Gradle
+
+    @Before
+    fun setup() {
+        gradle = mock<Gradle>()
+    }
+
+    @Test
+    fun `selector with null version of gradle`() {
+        val versionWithSelector = VersionWithSelector("1.0.0", VersionParser().transform("1.0.0"))
+        val selector = versionWithSelector.asSelector()
+        assertThat(selector.selector, equalTo("1.0.0"))
+    }
+
+    @Test
+    fun `selector with old version of gradle`() {
+        whenever(gradle.gradleVersion).thenReturn("3.4")
+        val versionWithSelector = VersionWithSelector("1.0.0", VersionParser().transform("1.0.0"), gradle)
+        val selector = versionWithSelector.asSelector()
+        assertThat(selector.selector, equalTo("1.0.0"))
+    }
+
+    @Test
+    fun `selector with new version of gradle`() {
+        whenever(gradle.gradleVersion).thenReturn("5.6.2")
+        val versionWithSelector = VersionWithSelector("1.0.0", VersionParser().transform("1.0.0"), gradle)
+        val selector = versionWithSelector.asSelector()
+        assertThat(selector.selector, equalTo("1.0.0"))
+    }
+
+}
