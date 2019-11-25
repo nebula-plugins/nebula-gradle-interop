cd075e381937c761c34fbaebe362e55753c7fc84
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2019-11-25 13:50:54 -0800

    VersionsIntegrationSpec: handle old versions of gradle with modern jvm

--- src/integTest/groovy/com/netflix/nebula/interop/VersionsIntegrationSpec.groovy
+++ src/integTest/groovy/com/netflix/nebula/interop/VersionsIntegrationSpec.groovy
@@ -2,6 +2,7 @@ package com.netflix.nebula.interop
 
 import nebula.test.IntegrationSpec
 import org.gradle.util.GradleVersion
+import spock.lang.IgnoreIf
 import spock.lang.Unroll
 
 class VersionsIntegrationSpec extends IntegrationSpec {
@@ -19,7 +20,8 @@ class VersionsIntegrationSpec extends IntegrationSpec {
     }
 
     @Unroll
-    def 'VersionWithSelector parses selector switching to proper DefaultVersionSelectorScheme constructor - Gradle #gradleVersionToUse'() {
+    @IgnoreIf({ jvm.isJava9Compatible() })
+    def 'VersionWithSelector parses selector switching to proper DefaultVersionSelectorScheme constructor - Legacy - Gradle #gradleVersionToUse'() {
         setup:
         gradleVersion = gradleVersionToUse
 
@@ -45,7 +47,36 @@ class VersionsIntegrationSpec extends IntegrationSpec {
         where:
         gradleVersionToUse << [
                 '4.4',
-                '4.5',
+                '4.5'
+        ]
+    }
+
+    @Unroll
+    def 'VersionWithSelector parses selector switching to proper DefaultVersionSelectorScheme constructor - modern - Gradle #gradleVersionToUse'() {
+        setup:
+        gradleVersion = gradleVersionToUse
+
+        buildFile << """
+        import com.netflix.nebula.interop.VersionWithSelector
+        import org.gradle.api.internal.artifacts.ivyservice.ivyresolve.strategy.VersionParser
+
+        task printVersion {
+            doLast {
+                println "Gradle version: " + project.gradle.gradleVersion
+                println "Version from Selector: " + new VersionWithSelector("1.0.0", new VersionParser().transform("1.0.0")).asSelector().selector
+            }
+        }
+        """
+
+        when:
+        def result = runTasksSuccessfully('printVersion')
+
+        then:
+        result.standardOutput.contains("Gradle version: " + gradleVersionToUse)
+        result.standardOutput.contains("Version from Selector: 1.0.0")
+
+        where:
+        gradleVersionToUse << [
                 '5.0',
                 '5.1',
                 '5.2',
